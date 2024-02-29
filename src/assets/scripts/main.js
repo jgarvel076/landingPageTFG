ede796c901eb1cb96f3d308ceeb63126772ea9a3
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2024-02-29 14:45:51 +0100

    Preparacion

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,19 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+
+/*boton cambio de fuente */
+
+let originalFont = true;
+
+function toggleFont() {
+    const body = document.querySelector('body');
+    console.log(body.style.fontFamily)
+    if (originalFont) {
+        body.style.fontFamily = "Inter";
+    } else {
+        body.style.fontFamily = "OpenDislexic, sans-serif";
+    }
+    originalFont = !originalFont;
+}
